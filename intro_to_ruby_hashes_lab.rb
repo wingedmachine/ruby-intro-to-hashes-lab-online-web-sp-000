eed6296f049d410afe8b200fc726d0243d10976a
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 01:10:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,8 +51,8 @@ def monopoly_with_fourth_tier
       names: {
         reading_railroad: {"mortgage_value" => "$100"},
         pennsylvania_railroad: {"mortgage_value" => "$200"},
-        b_and_o_railroad: {"mortgage_value" => "$100"},
-        shortline: {"mortgage_value" => "$100"},
+        b_and_o_railroad: {"mortgage_value" => "$400"},
+        shortline: {"mortgage_value" => "$800"},
       },
       rent_in_dollars: {
         one_piece_owned: 25,

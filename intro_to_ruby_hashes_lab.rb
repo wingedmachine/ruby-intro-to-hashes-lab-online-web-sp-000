174a0695e4b5496e90773398803c746d6368d0e5
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 01:09:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ def monopoly_with_third_tier
         reading_railroad: {},
         pennsylvania_railroad: {},
         b_and_o_railroad: {},
-        wert2_railroad: {},
+        shortline: {},
       },
       rent_in_dollars: {
         one_piece_owned: 25,
